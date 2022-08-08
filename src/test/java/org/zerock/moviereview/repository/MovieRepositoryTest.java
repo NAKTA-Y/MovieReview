01b4b0a9039ea7c76069f06ca3bfca1c72969140
nakta <nakta0511@gmail.com>
2022-08-08 11:42:46 +0900

    repository & Create test

---
+++ src/test/java/org/zerock/moviereview/repository/MovieRepositoryTest.java
@@ -0,0 +1,54 @@
+package org.zerock.moviereview.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+import org.zerock.moviereview.entity.Movie;
+import org.zerock.moviereview.entity.MovieImage;
+
+import java.util.UUID;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MovieRepositoryTest {
+
+    @Autowired MovieRepository movieRepository;
+
+    @Autowired MovieImageRepository imageRepository;
+
+    @Commit
+    @Transactional
+    @Test
+    public void insertMovies() {
+
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+
+            Movie movie = Movie.builder()
+                    .title("Movie..." + i)
+                    .build();
+
+            System.out.println("------------------------------------------");
+
+            movieRepository.save(movie);
+
+            int count = (int)(Math.random() * 5) + 1;
+
+            for (int j = 0; j < count; j++) {
+
+                MovieImage movieImage = MovieImage.builder()
+                        .uuid(UUID.randomUUID().toString())
+                        .movie(movie)
+                        .imgName("test" + j + ".jpg")
+                        .build();
+
+                imageRepository.save(movieImage);
+            }
+
+            System.out.println("-------------------------------------------");
+        });
+    }
+}
